049dfaa3e9c4e26c7ed6b2e187b86c545a61b9fa
harsh <harsh@HarshithBM>
2024-01-16 11:55:11 +0530

    version-2

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.demo.entities.Song;
 import com.example.demo.entities.Users;
+import com.example.demo.services.SongService;
 import com.example.demo.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
@@ -20,6 +25,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UsersController {
 @Autowired
 UsersService service;
+
+@Autowired
+SongService songService2;
 
 @GetMapping("/register")
 public String addUser(@ModelAttribute Users user)
@@ -41,7 +49,7 @@ return "home";
 @PostMapping("/validate")
 public String validate(@RequestParam("email") String email,
               @RequestParam("password") String password,
-              HttpSession session){
+              HttpSession session , Model model){
 
 if(service.validateUser(email,password)==true)
 {
@@ -53,6 +61,13 @@ return "adminHome";
 }
 else
 {
+	Users user = service.getUser(email);
+	boolean userStatus = user.isPremium();
+	List<Song> songsList = songService2.fetchAllSongs();
+	model.addAttribute("songs", songsList);
+	
+	model.addAttribute("isPremium",userStatus);
+	
 return "customerHome";
 }
 }
